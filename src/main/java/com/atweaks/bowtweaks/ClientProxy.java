d274ad814f421fa78c29ed38d923d850794ffea5
zzggxx09-byte <zzggxx09@gmail.com>
2026-07-28 00:12:56 +0300

    Update ClientProxy.java

--- src/main/java/com/atweaks/bowtweaks/ClientProxy.java
+++ src/main/java/com/atweaks/bowtweaks/ClientProxy.java
@@ -1,17 +1,15 @@
 package com.atweaks.bowtweaks;
 
 import cpw.mods.fml.client.registry.RenderingRegistry;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.RenderArrow;
 
 public class ClientProxy extends CommonProxy {
 
     @Override
     public void registerRenderers() {
-        // Використовуємо стандартний ванільний рендер стріли (вигляд як у звичайної стріли)
-        RenderingRegistry.registerEntityRenderingHandler(EntityArrowWooden.class,
-                new RenderArrow(Minecraft.getMinecraft().getRenderManager()));
-        RenderingRegistry.registerEntityRenderingHandler(EntityArrowIron.class,
-                new RenderArrow(Minecraft.getMinecraft().getRenderManager()));
+        // У 1.7.10 конструктор Render() без аргументів,
+        // RenderManager підставляється автоматично при реєстрації
+        RenderingRegistry.registerEntityRenderingHandler(EntityArrowWooden.class, new RenderArrow());
+        RenderingRegistry.registerEntityRenderingHandler(EntityArrowIron.class, new RenderArrow());
     }
 }
