e43b496c7bde8aeaa50563374194ce9067cc87b6
zzggxx09-byte <zzggxx09@gmail.com>
2026-07-27 23:24:47 +0300

    Update ItemProfessionalBow.java

--- src/main/java/com/atweaks/bowtweaks/ItemProfessionalBow.java
+++ src/main/java/com/atweaks/bowtweaks/ItemProfessionalBow.java
@@ -5,25 +5,72 @@ import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemBow;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.ArrowNockEvent;
 
 public class ItemProfessionalBow extends ItemBow {
+
+    private IIcon[] pullIcons;
 
     public ItemProfessionalBow() {
         this.setMaxStackSize(1);
         this.setCreativeTab(CreativeTabs.tabCombat);
         this.setUnlocalizedName("bowtweaks_professional_bow");
         this.setMaxDamage(ConfigHandler.proBowDurability);
+        this.setFull3D();
     }
 
     @Override
     public void registerIcons(IIconRegister iconRegister) {
-        this.itemIcon = iconRegister.registerIcon("minecraft:bow_standby");
+        this.itemIcon = IconHelper.register(iconRegister, "bowtweaks", "professional_bow_standby", "minecraft:bow_standby");
+
+        pullIcons = new IIcon[3];
+        pullIcons[0] = IconHelper.register(iconRegister, "bowtweaks", "professional_bow_pulling_0", "minecraft:bow_pulling_0");
+        pullIcons[1] = IconHelper.register(iconRegister, "bowtweaks", "professional_bow_pulling_1", "minecraft:bow_pulling_1");
+        pullIcons[2] = IconHelper.register(iconRegister, "bowtweaks", "professional_bow_pulling_2", "minecraft:bow_pulling_2");
+    }
+
+    @Override
+    public IIcon getIcon(ItemStack stack, int renderPass, EntityPlayer player, ItemStack usingItem, int useRemaining) {
+        if (usingItem == null) {
+            return this.itemIcon;
+        }
+
+        int ticksInUse = this.getMaxItemUseDuration(stack) - useRemaining;
+        int drawTicks = ConfigHandler.proBowDrawTicks;
+
+        if (ticksInUse >= drawTicks) {
+            return pullIcons[2];
+        } else if (ticksInUse > drawTicks * 0.66F) {
+            return pullIcons[1];
+        } else if (ticksInUse > 0) {
+            return pullIcons[0];
+        }
+        return this.itemIcon;
     }
 
     @Override
     public int getMaxItemUseDuration(ItemStack stack) {
         return 72000;
+    }
+
+    @Override
+    public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player) {
+        boolean hasAmmo = player.capabilities.isCreativeMode || player.inventory.hasItem(ModItems.itemArrowIron);
+
+        ArrowNockEvent event = new ArrowNockEvent(player, stack);
+        MinecraftForge.EVENT_BUS.post(event);
+        if (event.isCanceled()) {
+            return event.result;
+        }
+
+        if (hasAmmo) {
+            player.setItemInUse(stack, this.getMaxItemUseDuration(stack));
+            world.playSoundAtEntity(player, "bowtweaks:professional_bow_draw", 0.5F, 1.0F);
+        }
+        return stack;
     }
 
     @Override
@@ -49,7 +96,7 @@ public class ItemProfessionalBow extends ItemBow {
         }
 
         stack.damageItem(1, player);
-        world.playSoundAtEntity(player, "random.bow", 1.0F,
+        world.playSoundAtEntity(player, "bowtweaks:professional_bow_shoot", 1.0F,
                 1.0F / (this.itemRand.nextFloat() * 0.4F + 1.2F) + charge * 0.5F);
 
         if (creative) {
